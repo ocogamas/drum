39b52b2c5e79a80abba7592de53afeb1b82dc042
KONDO MASATO <(null)>
2018-04-27 08:24:09 +0900

    いい感じの３Dを描画

---
+++ Assets/Script/Spin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    [SerializeField] private GameObject go;
+
+	void Start () 
+    {
+		
+	}
+	
+
+	void Update () 
+    {
+        Vector3 axis = new Vector3 (1.0f, 1.0f, 0.0f);
+
+        float angle = 60f * Time.deltaTime;
+
+        Quaternion q = Quaternion.AngleAxis (angle, axis);
+
+        this.go.transform.rotation = q * this.transform.rotation;
+	}
+}
